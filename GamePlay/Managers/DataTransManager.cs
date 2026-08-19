b876d4ebf30585dfe374212180d1fbffaf541b79
MyslZhao <159505988+MyslZhao@users.noreply.github.com>
2026-08-19 15:29:47 +0800

    完善游戏逻辑
    对DataTransManager完善弹药贮存与自检逻辑，并据此进一步调整相关接口。

--- GamePlay/Managers/DataTransManager.cs
+++ GamePlay/Managers/DataTransManager.cs
@@ -1,37 +1,106 @@
-using UnityEngine;
-
+using System;
 using FlightFight.Shared.Enums;
+using UnityEngine;
+using System.Collections.Generic;
 
 namespace FlightFight.GamePlay.Managers
 {
     public class DataTransManager: MonoBehaviour
     {
-        private static DataTransManager Instance;
-
-        [Header("Debug模式")]
-        public bool _EnableDebug;
+        [Header("Test模式")]
+        [SerializeField]private bool EnableTest = false;
 
         [Header("玩家配置")]
-        [SerializeField] private static AmmoEnum[] _SelfAmmo = new AmmoEnum[5];
+        [Obsolete]
+        [SerializeField]private AmmoEnum[] _SelfAmmo = new AmmoEnum[5];
 
-        [SerializeField] private static AmmoEnum[] _EnemyAmmo = new AmmoEnum[5];
+        [Obsolete]
+        [SerializeField]private AmmoEnum[] _EnemyAmmo = new AmmoEnum[5];
+
+        private static DataTransManager _Instance;
+
+        private bool _IsReady = false;
+
+        private readonly Dictionary<PlaneIdentity, AmmoEnum[]> _AmmoLists = new();
+
+        private readonly Dictionary<PlaneIdentity, int> _AmmoNumbers = new();
+
+        internal static bool IsReady => _Instance._IsReady;
+
+        internal static AmmoEnum[] SelfAmmo => _Instance._AmmoLists[PlaneIdentity.SELF];
+
+        internal static AmmoEnum[] EnemyAmmo => _Instance._AmmoLists[PlaneIdentity.ENEMY];
 
         void Awake()
         {
-            if (Instance == null)
+            if (_Instance == null)
             {
-                Instance = this;
+                _Instance = this;
                 DontDestroyOnLoad(gameObject);
             }
             else
             {
                 Destroy(gameObject);
             }
+
+            if (EnableTest)
+            {
+                _SafeDictionalize();
+                foreach (var i in _AmmoNumbers.Keys)
+                {
+                    _AmmoNumbers[i] = 5;
+                }
+
+                _CheckReady();
+            }
+            else
+            {
+                foreach(var i in _AmmoLists.Keys)
+                {
+                    _AmmoNumbers.Add(i, 0);
+                }
+            }
         }
 
-        public static AmmoEnum[] SelfAmmo => _SelfAmmo;
-        
-        public static AmmoEnum[] EnemyAmmo => _EnemyAmmo;
+        private void _SafeDictionalize()
+        {
+#pragma warning disable CS0612
+            Debug.Assert(_SelfAmmo != null, "Your SelfAmmo is None!");
+            Debug.Assert(_EnemyAmmo != null, "Your EnemyAmmo is None!");
+
+            _AmmoLists.Add(PlaneIdentity.SELF, _SelfAmmo);
+            _AmmoLists.Add(PlaneIdentity.ENEMY, _EnemyAmmo);
+
+            _SelfAmmo = null;
+            _EnemyAmmo = null;
+#pragma warning restore CS0612
+        }
+
+        private void _CheckReady()
+        {
+            foreach (var i in _AmmoNumbers.Keys)
+            {
+                if (_AmmoNumbers[i] != 5)
+                {
+                    return;
+                }
+            }
+            _IsReady = true;
+        }
+
+        private void _DeployAmmo(PlaneIdentity identity, AmmoEnum ammo)
+        {
+            var _1 = _AmmoLists[identity];
+            _1[_AmmoNumbers[identity]] = ammo;
+            _AmmoNumbers[identity] += 1;
+        }
+
+        public void DeployAmmo(PlaneIdentity identity, AmmoEnum ammo)
+        {
+            _DeployAmmo(identity, ammo);
+
+            _CheckReady();
+        }
 
     }
 
